6cbe5751cacbadce87694d03a679b2f080bbe136
diegohenao819 <diegohenao819@gmail.com>
2024-06-19 16:46:40 -0500

    refactor: Update initialItems in constants.js

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,25 +1,16 @@
 export const secondaryButtons = [
-    "Mark all as complete",
-    "Mark all as incomplete",
-    "Reset to initial",
-    "Remove all items"
+  "Mark all as complete",
+  "Mark all as incomplete",
+  "Reset to initial",
+  "Remove all items"
 ]
 
 export const initialItems = [
-    {
-      id: 1,
-      name: "good mood",
-      packed: true,
-    },
-    {
-      id: 2,
-      name: "passport",
-      packed: false,
-    },
-    {
-      id: 3,
-      name: "phone charger",
-      packed: false,
-    },
-  ];
+  {
+    id: 1,
+    name: "Buy coffee",
+    packed: false,
+  },
+
+];
 
